9ecea4c7fa7a2b4f904baacd133e259b96d1b30a
dlekdlsll <dtp05093@naver.com>
2022-08-16 23:51:36 +0900

    Fix: 콘솔창 출력 폼 변경
    - 출력 폼 변경

--- FirstWeek/src/management.java
+++ FirstWeek/src/management.java
@@ -1,9 +1,15 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.UUID;
 
 public class management {
+	
+	static Map<String, Object> map = new HashMap<>();
 
 	public static void main(String[] args) {
 		int i = 0;
+		
 		do {
 			menu();
 			Scanner sc = new Scanner(System.in);
@@ -13,6 +19,7 @@ public class management {
 			switch (i) {
 				case 1: {
 					System.out.println("전체 레코드를 조회합니다.");
+					read();
 					break;
 				}
 				case 2: {
@@ -21,6 +28,10 @@ public class management {
 				}
 				case 3: {
 					System.out.println("신규 레코드를 추가합니다.");
+					 String name = sc.next();
+					 String phoneNumber = sc.next();
+					 String address = sc.next();
+					 insert(name, phoneNumber, address);
 					break;
 				}
 				case 4: {
@@ -48,6 +59,19 @@ public class management {
 		System.out.println("3) 신규 레코드 추가");
 		System.out.println("4) key를 기반으로 갱신");
 		System.out.println("5) key를 기반으로 삭제");
+	}
+	
+	public static void read() {
+		System.out.println("|                  id                  |  name  |   phoneNumber   |  address  |");
+		System.out.println("==============================================================================");
+		for (String key: map.keySet()) {
+			member value = (member) map.get(key);
+			System.out.println("| " + key + " |  " + value.getName() + "  |  " + value.getPhoneNumber() + "  |  " + value.getAddress() + "  |");
+		}
+	}
+	
+	public static void insert(String name, String phoneNumber, String address) {
+		map.put(UUID.randomUUID().toString(), new member(name, phoneNumber, address));
 	}
 
 }
